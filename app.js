9ee210e4d3c14ef4c26d8434fea5dc0980644b5c
Lena Tevar <13490448+LenaTevar@users.noreply.github.com>
2019-11-04 14:19:43 +0100

    Adding comments

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const { resolve } = require('path')
 const imdb = resolve('./lib/movies/movies.json')
 const xml = resolve('./lib/movies/movies.xml')
-
-
-console.log('app')
-console.log(imdb)
 let reviewer = require('./lib/reviewer')
+
 reviewer.init(imdb, xml)
 
 
